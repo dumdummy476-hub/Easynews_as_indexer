4a15031ddff311cfab9640632a92afd07ced9c01
dumdummy476-hub <dumdummy476@gmail.com>
2026-09-01 08:41:11 +0200

    test: reject sample and trailer suffixes

--- tests/test_mapping.py
+++ tests/test_mapping.py
@@ -17,3 +17,19 @@ def test_parent_query_tokens_filter_noise():
     data = {"data": [row("Movie 2025 2160p REMUX"), {**row("Other 2025 2160p"), "hash":"h2"}]}
     items = map_results(data, 1, "Movie", year=2025)
     assert [x["hash"] for x in items] == ["h1"]
+
+
+def test_sample_and_trailer_suffixes_are_rejected():
+    data = {"data": [
+        row("Movie.2025.2160p.WEB-DL-sample"),
+        {**row("Movie.2025.2160p.WEB-DL-trailer"), "hash":"h2"},
+        {**row("Movie.2025.2160p.WEB-DL"), "hash":"h3"},
+    ]}
+    items = map_results(data, 1, "Movie", year=2025)
+    assert [x["hash"] for x in items] == ["h3"]
+
+
+def test_sample_word_inside_real_title_is_not_rejected():
+    data = {"data": [row("The.Sample.2025.2160p.WEB-DL")]}
+    items = map_results(data, 1, "The Sample", year=2025)
+    assert [x["hash"] for x in items] == ["h1"]
